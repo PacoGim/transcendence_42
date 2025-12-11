7b761ef038bfb28055324018236ede029a54c1ea
Paco Gimeno <pacogim.pro@pm.me>
2025-12-11 08:07:12 +0100

    refacto (server) all server routes

--- services/server/srcs/backend/server.ts
+++ services/server/srcs/backend/server.ts
@@ -1,27 +1,28 @@
+/********************** Fastify **********************/
 import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-import path from 'path'
-import { existsSync, readFileSync } from 'fs'
-import sanitizeHtml from 'sanitize-html'
+import cookie from '@fastify/cookie'
 import fastifyStatic from '@fastify/static'
 import fastifyWebsocket from '@fastify/websocket'
+
+/********************** Libs **********************/
+import fs from 'fs'
+import path from 'path'
+
+/********************** Functions **********************/
 import __dirname, { setDirName } from './functions/dirname.fn.js'
 import { publicWatcher } from './services/publicWatcher.service.js'
-import cookie from '@fastify/cookie'
-import { authRoutes, metricsRoutes, userRoutes } from './routes/handler.route.js'
-import { totalHttpRequests } from './services/prometheus.service.js'
+
+/********************** Services **********************/
 import { log } from './logs.js'
-import fs from 'fs'
-import Lobby from './classes/Lobby.js'
-import { json_parse, json_stringify } from '../frontend/functions/json_wrapper.js'
-import { MessageType } from '../types/message.type.js'
+import { totalHttpRequests } from './services/prometheus.service.js'
 
-import { v4 as uuidv4 } from 'uuid'
+/********************** Routes **********************/
+import { authRoutes, metricsRoutes, userRoutes } from './routes/handler.route.js'
+import { apiRoutes } from './routes/api.route.js'
+import { gameRoutes } from './routes/game.route.js'
+import { routerRoute } from './routes/router.route.js'
 
-import { getHTML } from './functions/getHTML.fn.js'
-
-const MAX_MESSAGE_LENGTH = 150
-
-const validRoutes = ['home', 'about', 'login', 'options', 'register', 'dashboard', 'users', 'game', 'lobby', 'chat']
+setDirName(path.resolve())
 
 const fastify: FastifyInstance = Fastify({
 	https: {
@@ -29,12 +30,6 @@ const fastify: FastifyInstance = Fastify({
 		cert: fs.readFileSync(path.join(__dirname(), 'certs/cert.pem'))
 	}
 })
-
-const lobby = new Lobby()
-
-await fastify.register(fastifyWebsocket)
-
-// const fastify: FastifyInstance = Fastify()
 
 fastify.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply) => {
 	totalHttpRequests.inc({
@@ -44,145 +39,21 @@ fastify.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply) => 
 	})
 })
 
-fastify.register(cookie)
-
-setDirName(path.resolve())
-publicWatcher()
-
 fastify.register(fastifyStatic, {
 	root: path.join(__dirname(), 'dist/public'),
 	prefix: '/'
 })
 
-await metricsRoutes(fastify)
-await authRoutes(fastify)
-await userRoutes(fastify)
+fastify.register(cookie)
+await fastify.register(fastifyWebsocket)
+metricsRoutes(fastify)
+authRoutes(fastify)
+userRoutes(fastify)
+apiRoutes(fastify)
+gameRoutes(fastify)
+routerRoute(fastify)
 
-fastify.post('/api/auth', async (req, reply) => {
-	const { code } = json_parse(req.body)
-
-	const url =
-		'https://api.intra.42.fr/oauth/token?' +
-		new URLSearchParams({
-			client_id: 'u-s4t2ud-9f30b2430e51c381ae5e38158295eef89230a74b070231a798bd1bcb7a01709c',
-			grant_type: 'authorization_code',
-			client_secret: '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',
-			code,
-			redirect_uri: 'https://localhost/register',
-			state: uuidv4()
-		})
-
-	const token = await fetch(url, { method: 'POST' })
-		.then(res => res.json())
-		.then(res => res?.access_token)
-
-	if (token) {
-		const infoFetch = await fetch('https://api.intra.42.fr/v2/me', {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		})
-			.then(res => res.json())
-			.then(res => {
-				const { email, login, first_name, last_name } = res
-				return { email, login, firstName: first_name, lastName: last_name }
-			})
-		return reply.send(infoFetch)
-	} else {
-		return reply.send({ status: 413 }).status(413)
-	}
-})
-
-fastify.get('/api/hello', (req, reply) => {
-	reply.send({ type: 'message', message: 'hello' })
-})
-
-fastify.get('/api/lobby', (req, reply) => {
-	reply.send(lobby)
-})
-
-fastify.get('/api/user', (req, reply) => {
-	const query: any = req.query
-	const { userId } = query
-	const cleanId = sanitizeHtml(userId)
-	const user = lobby.getUser(sanitizeHtml(cleanId))
-	if (!user) return reply.code(403).send({ error: `userId ${cleanId} invalid` })
-	return reply.send({ userId: cleanId, pseudo: user.pseudo })
-})
-
-fastify.post('/api/lobby', (req, reply) => {
-	const body: any = req.body
-	const { pseudo } = body
-	const cleanPseudo = sanitizeHtml(pseudo)
-	const payload = lobby.addUser(cleanPseudo)
-	if (payload.id === '0') return reply.code(403).send({ error: `${cleanPseudo} already taken` })
-	return reply.send(payload)
-})
-
-fastify.delete('/api/lobby', (req, reply) => {
-	const body: any = req.body
-	const { userId } = body
-	const cleanId = sanitizeHtml(userId)
-	const user = lobby.getUser(cleanId)
-	if (!user) return reply.code(403).send({ error: `userId ${cleanId} invalid` })
-	lobby.removeUser(user)
-	reply.send({ success: true })
-})
-
-fastify.get('/api/ws', { websocket: true }, (socket, req) => {
-	const query: any = req.query
-	console.log(query)
-	const { userId } = query
-	const cleanId = sanitizeHtml(userId)
-	console.log(`Connexion avec userId='${cleanId}'`)
-	const user = lobby.getUser(cleanId)
-	if (!user) return socket.close()
-	console.log('Socket closed')
-	lobby.refreshWebsocket(cleanId, socket)
-	socket.on('message', (raw: any) => {
-		const rawString = sanitizeHtml(raw.toString())
-		// Vérifier la taille maximale
-		if (rawString.length > MAX_MESSAGE_LENGTH) {
-			console.warn(`Message too long from ${cleanId}, ignored`)
-			socket.send(json_stringify({ type: 'error', text: `message too long (${MAX_MESSAGE_LENGTH} bytes max for the json)` }))
-			return
-		}
-		const msg = json_parse(rawString) as MessageType
-		if (!msg) {
-			console.warn(`Json invalid from ${cleanId}, ignored`)
-			socket.send(json_stringify({ type: 'error', text: `Invalid json format`, timestramp: Date.now() }))
-			return
-		}
-		lobby.handleMessage(user, msg)
-	})
-})
-
-fastify.route({
-	method: 'GET',
-	url: '/:route',
-	exposeHeadRoute: false,
-	handler: async (req: FastifyRequest, reply: FastifyReply) => {
-		let route = (req.params as { route: string | undefined })['route'] || ''
-		const type = req.headers.type as string
-		if (route === '') route = 'home'
-		if (validRoutes.includes(route)) {
-			const html = await getHTML(route, type || 'render')
-			if (html) return reply.type('text/html').send(html)
-			else {
-				const html = await getHTML(route, 'error', 404)
-				return reply.type('text/html').send(html)
-			}
-		} else {
-			const filePath = path.join(__dirname(), 'dist/public/', route)
-			if (existsSync(filePath)) {
-				const fileData = readFileSync(filePath, { encoding: 'utf8' })
-				return reply.type('text/javascript').send(fileData)
-			}
-		}
-		const html = await getHTML(route, 'error', 404)
-		return reply.type('text/html').send(html)
-	}
-})
+publicWatcher()
 
 const start = async () => {
 	try {
