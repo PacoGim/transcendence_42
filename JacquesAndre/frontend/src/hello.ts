6112f2b28f4c538998f53628c495298f787a8479
jbourdoi <jbourdoi@student.42.fr>
2026-01-13 16:32:26 +0100

    feat(tournament) almost done

--- JacquesAndre/frontend/src/hello.ts
+++
@@ -1,7 +0,0 @@
-export default function hello(element: HTMLDivElement)
-{
-	fetch("https://localhost:3000/api/hello")
-		.then(res=>res.json())
-		.then(json=>element.innerText = JSON.stringify(json))
-		.catch(e=>console.error(e))
-}
--- JacquesAndre/frontend/src/utils/json.ts
+++
@@ -1,8 +0,0 @@
-import {json_parse, json_stringify} from "../../../shared/json_wrapper.js"
-
-
-// export const stringify = JSON.stringify;
-// export const parse = JSON.parse;
-
-export const stringify = json_stringify;
-export const parse = json_parse;
