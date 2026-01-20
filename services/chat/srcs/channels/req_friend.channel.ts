6d61c070c7ddd42fe76ee2ab1527023d82674e12
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-20 13:44:26 +0100

    feat(friend request): stop from sending friend request if the other user is blocked

--- services/chat/srcs/channels/req_friend.channel.ts
+++ services/chat/srcs/channels/req_friend.channel.ts
@@ -4,6 +4,32 @@ import { BunSocketType } from '../types/bunSocket.type'
 import { ClientType } from '../types/client.type'
 import { SocketDataType } from '../types/socketData.type'
 
+async function isClientBlocked(blocker: string, blocked: string): Promise<any> {
+	const res = await dbPostQuery({
+		endpoint: 'dbGet',
+		query: {
+			verb: 'SELECT',
+			sql: 'SELECT * FROM blocks WHERE (blocker_username = ? AND blocked_username = ?) OR (blocker_username = ? AND blocked_username = ?)',
+			data: [blocker, blocked, blocked, blocker]
+		}
+	})
+	console.log('INSERT FRIEND REQUEST --- isClientBlocked: ', res)
+	return res
+}
+
+async function insertFriendRequest(from: string, to: string): Promise<any> {
+	const res = await dbPostQuery({
+		endpoint: 'dbRun',
+		query: {
+			verb: 'INSERT',
+			sql: 'INSERT INTO friend_requests (from_username, to_username) VALUES (?, ?)',
+			data: [from, to]
+		}
+	})
+	console.log('INSERT FRIEND REQUEST --- insertFriendRequest: ', res)
+	return res
+}
+
 export async function reqFriendChannel(ws: BunSocketType, data: SocketDataType) {
 	let clientFound: ClientType
 	console.log('Friends request')
@@ -14,16 +40,34 @@ export async function reqFriendChannel(ws: BunSocketType, data: SocketDataType) 
 	}
 	console.log('Client Found: ', clientFound)
 	if (clientFound) {
-		const res = await dbPostQuery({
-			endpoint: 'dbRun',
-			query: {
-				verb: 'INSERT',
-				sql: 'INSERT INTO friend_requests (from_username, to_username) VALUES (?, ?)',
-				data: [ws.data.username, clientFound.username]
-			}
-		})
-		if (res.status >= 400) console.log('status: ', res.status, 'message: ', res.message)
+		// if blocked, cannot send request -> SELECT from blocks, if here stop if not here INSERT send request
+		// if already in friendships, cannot send request -> SELECT from friendships, if here stop if not here INSERT send request
+		// if A already sent request to B, cannot send request again from B -> DELETE from friend_requests and INSERT to friendships
+		let res = await isClientBlocked(ws.data.username, clientFound.username)
+		if (res.status >= 400 && res.status !== 404)
+		{
+			data.msg = res.message
+			data.type = 'error'
+			ws.send(JSON.stringify(data))
+			return
+		}
+		else if (res.data) {
+			data.msg = 'Cannot send friend request. One of the users has blocked the other.'
+			data.type = 'error'
+			ws.send(JSON.stringify(data))
+			return
+		}
+		console.log('User not blocked, send friend request')
+
+		res = await insertFriendRequest(ws.data.username, clientFound.username)
+		if (res.status >= 400) {
+			data.msg = res.message
+			data.type = 'error'
+			ws.send(JSON.stringify(data))
+			return
+		}
 		console.log('Friend request sent to DB: ', res)
+
 		data.msg = clientFound.username
 		data.type = 'req-friend'
 		clientFound.socket.send(JSON.stringify(data))
