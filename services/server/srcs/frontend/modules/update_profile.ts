71cbb6af20061c8ed7191e74eea52bd2dcfb594c
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-22 09:38:07 +0100

    feat(2fa): wip show/hide modal with fake validation code

--- services/server/srcs/frontend/modules/update_profile.ts
+++ services/server/srcs/frontend/modules/update_profile.ts
@@ -6,12 +6,118 @@ let trackEvent = false
 const $page: HTMLElement = document.querySelector('page[type=update_profile]')!
 const $usernameInput: HTMLInputElement = document.querySelector('input[name="username"]')!
 
+function open2FAModal(modal: HTMLDivElement, overlay: HTMLDivElement, codeInput: HTMLInputElement, modalError: HTMLDivElement) {
+	modal.classList.remove('hidden')
+	overlay.classList.remove('hidden')
+	codeInput.value = ''
+	modalError.classList.add('hidden')
+}
+
+function close2FAModal(modal: HTMLDivElement, overlay: HTMLDivElement) {
+	modal.classList.add('hidden')
+	overlay.classList.add('hidden')
+}
+
+function send2FACode(): boolean {
+	// fake for testing purposes
+	const success = Math.random() > 0.2
+	if (success) {
+		console.log('2FA code sent successfully')
+		return true
+	}
+	console.log('Failed to send 2FA code')
+	return false
+}
+
+function validate2FACode(codeInput: HTMLInputElement) {
+	// fake for testing purposes
+	const validate2FABtn = $page.querySelector('#twofa-validate-btn') as HTMLButtonElement
+	const modalError = $page.querySelector('#twofa-error') as HTMLDivElement
+	const $toggle2FABtn = $page.querySelector('#twofa') as HTMLInputElement
+	const modal = $page.querySelector('#twofa-modal') as HTMLDivElement
+	const overlay = $page.querySelector('#twofa-modal-overlay') as HTMLDivElement
+
+	validate2FABtn.addEventListener('click', (e) => {
+		const code = codeInput.value.trim()
+		if (!/^\d{6}$/.test(code)) {
+			console.log('Invalid 2FA code format')
+			displayModalError(modalError, 'Invalid code format. Please enter a 6-digit code.')
+			return
+		}
+		// fake validation
+		const isValid = Math.random() > 0.2
+		if (!isValid) {
+			console.log('Incorrect 2FA code')
+			displayModalError(modalError, 'Incorrect code. Please try again.')
+			return
+		}
+
+		console.log('2FA code validated successfully')
+
+		$toggle2FABtn.checked = !$toggle2FABtn.checked
+		console.log('toggle2FABtn checked IF CODE VALIDATED:', $toggle2FABtn.checked)
+		toggle2FAState($toggle2FABtn)
+		console.log('toggle2FABtn checked IF CODE VALIDATED x2:', $toggle2FABtn.checked)
+
+		close2FAModal(modal, overlay)
+	})
+}
+
+function displayModalError(modalError: HTMLDivElement, message: string) {
+	modalError.textContent = message
+	modalError.classList.remove('hidden')
+}
+
+function toggle2FAState(toggle2FABtn: HTMLInputElement) {
+	toggle2FABtn.textContent = toggle2FABtn.checked ? '2FA enabled' : '2FA disabled'
+	console.log('toggle2FABtn checked AFTER CODE VALIDATED (from x2):', toggle2FABtn.checked)
+}
+
+function toggle2FA(toggle2FABtn: HTMLInputElement, closeModalBtn: HTMLButtonElement, modal: HTMLDivElement, overlay: HTMLDivElement, codeInput: HTMLInputElement, modalError: HTMLDivElement) {
+	toggle2FABtn.addEventListener('change', (e) => {
+		e.preventDefault()
+		toggle2FABtn.checked = !toggle2FABtn.checked
+		console.log('toggle2FABtn checked AT START:', toggle2FABtn.checked)
+
+		open2FAModal(modal, overlay, codeInput, modalError)
+		const success = send2FACode()
+		if (!success) {
+			displayModalError(modalError, 'Error sending 2FA code. Please try again.')
+			// what should happen when failing to send code?
+			return
+		}
+		// on validate, check code server-side, if ok -> enable/disable 2fa for user in db
+		validate2FACode(codeInput)
+	})
+	closeModalBtn.addEventListener('click', () =>
+		close2FAModal(modal, overlay)
+	)
+	overlay.addEventListener('click', () =>
+		close2FAModal(modal, overlay)
+	)
+}
+
 function handleUpdateProfile() {
 	const $submitBtn = document.querySelector('span[type="submit"]') as HTMLElement
 	const $avatarInput = $page.querySelector('input[name="avatar"]') as HTMLInputElement
 	const $avatarPreview = $page.querySelector('#avatarPreview') as HTMLImageElement
 	const $resetAvatarBtn = $page.querySelector('#resetAvatarButton') as HTMLButtonElement
+	
 	resetAvatarButton($resetAvatarBtn, $avatarInput, $avatarPreview)
+
+	const $toggle2FABtn = $page.querySelector('#twofa') as HTMLInputElement
+	const modal = $page.querySelector('#twofa-modal') as HTMLDivElement
+	const overlay = $page.querySelector('#twofa-modal-overlay') as HTMLDivElement
+	const closeModalBtn = $page.querySelector('#twofa-modal-close') as HTMLButtonElement
+	// const validate2FABtn = $page.querySelector('#twofa-validate-btn') as HTMLButtonElement
+	const resend2FABtn = $page.querySelector('#twofa-resend-btn') as HTMLButtonElement
+	const codeInput = $page.querySelector('#twofa-code-input') as HTMLInputElement
+	const modalError = $page.querySelector('#twofa-error') as HTMLDivElement
+
+	toggle2FA($toggle2FABtn, closeModalBtn, modal, overlay, codeInput, modalError)
+
+	// weird bug when code validation to disable 2fa: code validated but checkbox true, sometimes false
+	// in 'on change' if 2fa checked/unchecked -> modif user from table to enable/disable 2fa
 
 	if (trackEvent === false) {
 		trackEvent = true
